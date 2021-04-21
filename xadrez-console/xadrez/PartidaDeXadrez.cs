1d6b764fc9d7ba49baf089aacad3125304c34f67
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2021-04-20 23:07:45 -0300

    Jogada Especial Promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -141,13 +141,28 @@ namespace xadrez_console.xadrez
 
         public void RealizaJogada(Posicao origem, Posicao destino)
         {
-
+           
             Peca pecaCapturada = ExecutaMovimento(origem, destino);
             if (EstaEmXeque(JogadorAtual))
             {
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroExceptions("Voce não pode se colocar em Xeque!");
             }
+
+            Peca p = Tab.Peca(destino);
+            // Jogada Especial Promocao
+            if (p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) ||(p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab , p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -161,7 +176,7 @@ namespace xadrez_console.xadrez
             Turno++;
             MudaJogador();
 
-            Peca p = Tab.Peca(destino);
+            
 
             // Jogada Especial en Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
